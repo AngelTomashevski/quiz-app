357be83882441e8a7e01ad8e238c4aac27934918
AngelTomashevski <100373215+AngelTomashevski@users.noreply.github.com>
2022-12-08 11:31:53 +0100

    some new styling and more reusable code

--- src/components/answers/AnswersList.jsx
+++ src/components/answers/AnswersList.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import classes from "./AnswersList.module.css"
-import { useDispatch, useSelector } from 'react-redux';
-import { checkAnswers } from '../../feature/quizSlice';
-
-
+import React from "react";
+import classes from "./AnswersList.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { checkAnswers } from "../../feature/quizSlice";
 
 const AnswersList = () => {
-    const {questions, index} = useSelector(state=>state.quiz);
-    const dispatch = useDispatch();
+  const { questions, index } = useSelector((state) => state.quiz);
+  const dispatch = useDispatch();
 
-   
- const { question, incorrectAnswers, correctAnswer } = questions[index];
+  const { question, incorrectAnswers, correctAnswer } = questions[index];
 
- let answers = [...incorrectAnswers];
-  const tempIndex = Math.floor(Math.random() * 4);
+  let answers = [...incorrectAnswers];
+  const tempIndex = Math.floor(Math.random() * questions.length);
   if (tempIndex === 3) {
     answers.push(correctAnswer);
   } else {
@@ -22,31 +19,28 @@ const AnswersList = () => {
   }
 
   const correlation = (condition) => {
-    dispatch(checkAnswers(condition))
-  }
+    dispatch(checkAnswers(condition));
+  };
 
   return (
-     <article className={classes.container}>
-          <h2>{"Question " + (index + 1) + ": " + question}</h2>
-          <div className={classes["btn-container"]}>
-            {answers.map((answer,index)=>{
-              const {id} = questions[index]
-              return (
-                <button
-                  key={id}
-                  className={classes["answer-btn"]}
-                  onClick={() =>
-                    correlation(correctAnswer === answer)}>
-                      {(index +1)  + ") " + answer}
-                      </button>
-                 )
-              })
-            }
-          </div>
-        </article>
-  )
+    <article className={classes.container}>
+      <h2>{"Question " + (index + 1) + ": " + question}</h2>
+      <div className={classes["btn-container"]}>
+        {answers.map((answer, index) => {
+          const { id } = questions[index];
+          return (
+            <button
+              key={id}
+              className={classes["answer-btn"]}
+              onClick={() => correlation(correctAnswer === answer)}
+            >
+              {`${index + 1}) ${answer}`}
+            </button>
+          );
+        })}
+      </div>
+    </article>
+  );
+};
 
-   
-}
-
-export default AnswersList
+export default AnswersList;
